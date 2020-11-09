06541901fee9ecb8c470a38d111ed4a47390967f
RDOToole89 <roibinotoole@gmail.com>
2020-11-09 14:57:00 +0100

    add route for serving a single space by id

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const User = require("../models/").user;
 const Space = require("../models/").space;
+const Story = require("../models").story;
 
 console.log("USER", User);
 console.log("Space", Space);
@@ -10,6 +11,18 @@ const router = new Router();
 router.get("/", async (req, res, next) => {
   try {
     const spaces = await Space.findAll();
+
+    res.json(spaces);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const spaces = await Space.findByPk(id, { include: [Story] });
 
     res.json(spaces);
   } catch (e) {
